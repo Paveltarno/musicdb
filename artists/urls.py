fb8b4ed1e86dca8b9e9050096380bfeefdb8f369
Pavel Tarno <paveltarno@gmail.com>
2015-02-14 23:02:24 +0200

    Still working [=

--- artists/urls.py
+++ artists/urls.py
@@ -3,4 +3,5 @@ from artists import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<artist_name>\d+)/vote/$', views.add_artist, name='add_artist'),
 )
--- artists/views.py
+++ artists/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from artists.models import *
+from artists.utils import ITunesAdapter
+import json
 
 def index(request):
   return render(request, 'artists/index.html')
+
+def add_artist(request, artist_name):
+  artist_added = False
+
+  # Query the artist
+  artist = Artist.create_from_source(artist_name, ITunesAdapter)
+  if artist:
+    artist.save()
+
+    # Check for the artists albums
+    albums = Album.create_albums_from_source_by_artist(artist.external_id, ITunesAdapter)
+    for album in albums:
+      artist.album_set.add(album)
+    artist_added = True
+
+  message = "Albums Added." if artist_added else "Artist not found."
+  return HttpResponse(json.dumps({'message': message}), content_type="application/json")
