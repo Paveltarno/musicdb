d26ab5dc9f7d61f057be63ae863fe06166bd43be
Pavel Tarno <paveltarno@gmail.com>
2015-02-16 19:41:39 +0200

    Fixed production settings file

--- musicdb/wsgi.py
+++ musicdb/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musicdb_prod.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production.py")
 
 # TODO: Set env vars here
 
